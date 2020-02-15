9b59077443fd20a064a03e55cae71f5b83d90052
Dan White <github@bluetufa.com>
2020-02-15 15:00:03 -0700

    zsh support

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -11,3 +11,15 @@ done
 
 mkdir -p ~/src/bin
 
+[[ -d ~/.oh-my-zsh ]] || install-oh-my-zsh 
+
+[[ -f ~/.local/share/nvim/site/autoload/plug.vim ]] || install-vim-plug
+
+function install-vim-plug() {
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+function install-oh-my-zsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
