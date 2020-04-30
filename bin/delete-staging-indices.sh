540c56c0bb359eda178d4296a24d866fe6bfbd48
Dan White <github@bluetufa.com>
2020-04-29 23:01:41 -0600

    white space

--- bin/delete-staging-indices.sh
+++ bin/delete-staging-indices.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for line in $(curl 'https://search-stagingme-76yrkk27nacwtjeq6d7vf7by5y.us-west-2.es.amazonaws.com/_cat/indices?v' | awk '{print $3 }' | grep '^activity.');do
+for line in $(curl 'https://search-stagingme-76yrkk27nacwtjeq6d7vf7by5y.us-west-2.es.amazonaws.com/_cat/indices?v' | awk '{print $3 }' | grep '^activity.'); do
   curl -XDELETE "https://search-stagingme-76yrkk27nacwtjeq6d7vf7by5y.us-west-2.es..amazonaws.com/${line}"
   echo $line
 done
