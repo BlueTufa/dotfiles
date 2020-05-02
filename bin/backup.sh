04143e057f129bc782b7d5176f3660ee9ce217e3
Dan White <github@bluetufa.com>
2020-05-02 09:12:59 -0600

    bug

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 cryptsetup open /dev/disk/by-uuid/3fa1a48e-fcc6-4ead-bb73-0157f86223f4 backup01
 mount /dev/mapper/backup01 /mnt/backup01
-sudo nohup rsync -aAXv / --exclude={"/srv/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt/backup01 </dev/null>
+sudo nohup rsync -aAXv / --exclude={"/srv/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt/backup01 </dev/null
