762bea85f9d54defb15ad80482e1bf621caacffd
Dan White <github@bluetufa.com>
2020-05-02 11:29:03 -0600

    significant upgrades to backup file

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,4 +1,16 @@
 #! /usr/bin/env bash
-cryptsetup open /dev/disk/by-uuid/3fa1a48e-fcc6-4ead-bb73-0157f86223f4 backup01
-mount /dev/mapper/backup01 /mnt/backup01
-sudo nohup rsync -aAXv / --exclude={"/srv/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt/backup01 </dev/null
+
+DISK_UUID=3fa1a48e-fcc6-4ead-bb73-0157f86223f4
+MOUNT_PATH=/mnt/backup01
+
+# command must be run as sudo, the whole thing
+if [[ $(whoami) != "root" ]]; then
+  echo "You must run this backup script as root for hardware access."
+  exit 1
+fi
+
+cryptsetup status backup01
+[[ $? -eq 0 ]] || cryptsetup open /dev/disk/by-uuid/${DISK_UUID} backup01
+grep -qs '/dev/mapper/backup01' /proc/mounts
+[[ $? -eq 0 ]] || mount /dev/mapper/backup01 ${MOUNT_PATH}
+nohup rsync -aAXv / --exclude={"/var/*","/srv/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} ${MOUNT_PATH} </dev/null
