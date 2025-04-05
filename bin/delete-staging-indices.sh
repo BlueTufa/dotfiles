1c187e40b6a650b0f2a538062cb124e6f170131f
BlueTufa <github@bluetufa.com>
2025-04-05 12:55:54 -0600

    more cleanup 2

--- bin/delete-staging-indices.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-for line in $(curl 'https://search-stagingme-76yrkk27nacwtjeq6d7vf7by5y.us-west-2.es.amazonaws.com/_cat/indices?v' | awk '{print $3 }' | grep '^activity.'); do
-  curl -XDELETE "https://search-stagingme-76yrkk27nacwtjeq6d7vf7by5y.us-west-2.es..amazonaws.com/${line}"
-  echo $line
-done
-
--- bin/macos-bootstrap.sh
+++ bin/macos-bootstrap.sh
@@ -4,17 +4,14 @@
 # Note that this file is not particularly useful assuming you plan to use brew
 # to install git, etc.  
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew bundle
 
 xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
 
 sudo sysadminctl -afpGuestAccess off
 sudo sysadminctl -smbGuestAccess off
 sudo sysadminctl -guestAccount off
+
 sudo scutil --set HostName $(pwgen 32 1)
 sudo scutil --set ComputerName $(pwgen 32 1)
-# defaults write NSGlobalDomain AppleHighlightColor "1.000000 0.874510 0.701961 Orange"
-# defaults write NSGlobalDomain AppleAccentColor 1
-# defaults write NSGlobalDomain AppleInterfaceStyle Dark
