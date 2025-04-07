7bab06d2a28cdf3bd504d8e813aca8dca08cdb93
Dan White <152660859+dan-white3@users.noreply.github.com>
2025-04-07 10:12:09 -0600

    installer cleanup (#94)
    * installer cleanup
    
    * fix cicd
    
    ---------
    
    Co-authored-by: BlueTufa <github@bluetufa.com>

--- installer.sh
+++ installer.sh
@@ -1,31 +1,6 @@
 #! /bin/bash
 
 # this file is idempotent and can safely be run many times. 
-# However, note that the backup function may overwrite previous file backups.
-
-install-vim-plug () {
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-}
-
-install-oh-my-zsh () {
-  make-backup ~/.zshrc
-  echo "Remember to type exit after the oh-my-zsh install is complete"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  sh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-  sh -c "git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab"
-
-  ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
-}
-
-install-fisher-plugins () {
-  fish -c "curl -sL https://git.io/fisher | source"
-  fish -c "fisher install jorgebucaran/fisher"
-  fish -c "fisher install jorgebucaran/nvm.fish"
-  fish -c "fisher install evanlucas/fish-kubectl-completions"
-  fish -c "fisher install patrickf1/fzf.fish"
-  fish -c "fisher install brgmnn/fish-docker-compose"
-}
 
 make-backup () {
   # if the file exists and is not a symlink...
@@ -37,40 +12,76 @@ make-backup () {
   fi
 }
 
-# os-independent config directories
+install-nvim () {
+  echo "Configuring nvim and installing vim-plug"
+  mkdir -p ~/.config/nvim/lua
+  make-backup ~/.config/nvim/init.lua
+  make-backup ~/.config/nvim/lua/plugins.lua
+
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  
+  ln -sf $(pwd)/nvim/init.lua ~/.config/nvim/init.lua
+  ln -sf $(pwd)/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
+
+  nvim -c ':PlugInstall' +qa
+}
+
+install-oh-my-zsh () {
+  make-backup ~/.zshrc
+  echo "Remember to type exit after the oh-my-zsh install is complete"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+  sh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  sh -c "git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab"
+
+  ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
+}
+
+install-fish () {
+  if ! command -v fish &> /dev/null
+  then
+    echo "Fish shell not found"
+    exit 0
+  fi
+
+  mkdir -p ~/.config/fish
+  make-backup ~/.config/fish/config.fish 
+  ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
+
+  for file in fish/.{abbr,functions,exports,aliases,*$(uname)}
+  do
+    make-backup ~/.config/${file}
+    echo "Linking ${file}..."
+    ln -sf $(pwd)/${file} ~/.config/${file}
+  done
+
+  fish -c "curl -sL https://git.io/fisher | source"
+  fish -c "fisher install jorgebucaran/fisher"
+  fish -c "fisher install jorgebucaran/nvm.fish"
+  fish -c "fisher install evanlucas/fish-kubectl-completions"
+  fish -c "fisher install patrickf1/fzf.fish"
+  fish -c "fisher install brgmnn/fish-docker-compose"
+}
+
 mkdir -p ~/src/bin
-mkdir -p ~/.config/fish
-mkdir -p ~/.config/nvim/lua
-
-# comment this out if you don't want to use oh-my-zsh or zsh support
-[[ -d ~/.oh-my-zsh ]] || install-oh-my-zsh
-
-# install vim-plug if not already present
-[[ -f ~/.local/share/nvim/site/autoload/plug.vim ]] || install-vim-plug
-
-make-backup ~/.config/fish/config.fish 
-make-backup ~/.config/nvim/init.lua
-
-ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
-ln -sf $(pwd)/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf $(pwd)/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
-ln -sf $(pwd)/starship.toml ~/.config/starship.toml
+touch ~/.hushlogin
 
 if [[ $(uname) == "Darwin" ]]; then
   [[ -d ~/Library/KeyBindings/ ]] || mkdir -p ~/Library/KeyBindings/
   cp ./macos/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/
-  brew bundle
+  brew bundle --cleanup
 fi
 
-# if fish is present, go ahead and install fisher and plugins
-[[ $(which fish) ]] && install-fisher-plugins
+# starship is the default prompt on zsh and fish
+make-backup ~/.config/starship.toml
+ln -sf $(pwd)/starship.toml ~/.config/starship.toml
 
-for file in fish/.{abbr,functions,exports,aliases,*$(uname)}
-do
-  make-backup ~/.config/${file}
-  echo "Linking ${file}..."
-  ln -sf $(pwd)/${file} ~/.config/${file}
-done
+# comment this out if you don't want nvim
+[[ -f ~/.local/share/nvim/site/autoload/plug.vim ]] || install-nvim
 
-touch ~/.hushlogin
+# comment this out if you don't want to use oh-my-zsh or zsh support
+[[ -d ~/.oh-my-zsh ]] || install-oh-my-zsh
+
+# comment this out if you don't want fish support
+install-fish
 
