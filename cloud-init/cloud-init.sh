2ee6b834ca3bfc5e11a2becb165f39fffef8be70
Dan White <BlueTufa@users.noreply.github.com>
2025-04-10 08:20:01 -0600

    cloud init bugs (#100)

--- cloud-init/cloud-init.sh
+++ cloud-init/cloud-init.sh
@@ -10,7 +10,7 @@ cloud-localds --filesystem=iso9660 seed.iso user-data.yml meta-data.yml
 
 virt-install --name fedora-cloud-01 --memory 16384 --vcpus 4 \
    --disk path=/kvmpool/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2,format=qcow2,bus=virtio \
-   --disk path=/kvmpool/images/seed.iso,device=cdrom,bus=sata \
+   --disk path=$(pwd)/seed.iso,device=cdrom,bus=sata \
    --os-variant fedora37 \
    --graphics none --import --noautoconsole
 
--- cloud-init/user-init.sh
+++ cloud-init/user-init.sh
@@ -10,6 +10,5 @@ echo "export WRK_SRC=$HOME/src" >> $HOME/.zsh/.exportsLocal
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.zprofile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.zprofile
 echo 'eval "$(pyenv init --path)"' >> $HOME/.zprofile
-chsh -s $(which zsh)
 # required python build dependencies
 sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-devel libnsl2-devel
