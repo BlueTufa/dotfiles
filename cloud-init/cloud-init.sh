9c0a89314a520ed271b3185f3e260cf35a40a9ac
BlueTufa <github@bluetufa.com>
2025-04-10 20:50:51 -0600

    cloud init tweaks

--- cloud-init/cloud-init.sh
+++ cloud-init/cloud-init.sh
@@ -4,15 +4,17 @@ export INSTANCE_ID=${1:-"vm-fedora-01"}
 export VCPUS=${2:-2}
 export MEMORY=${3:-4096}
 
-echo $INSTANCE_ID
-echo $VCPUS
-echo $MEMORY
 # requires install of virt-install, cloud-image-utils, and cloud-init
 
 [[ -f seed.iso ]] && rm seed.iso
+[[ -f user-data.named.yml ]] && rm  user-data.named.yml
+[[ -f meta-data.named.yml ]] && rm  meta-data.named.yml
+
 cloud-init schema --config-file user-data.yml
 cloud-init schema --config-file meta-data.yml
-cloud-localds --filesystem=iso9660 seed.iso user-data.yml meta-data.yml
+/bin/cat user-data.yml sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > user-data.named.yml
+/bin/cat meta-data.yml sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > meta-data.named.yml
+cloud-localds --filesystem=iso9660 seed.iso user-data.named.yml meta-data.named.yml
 
 virt-install --name ${INSTANCE_ID} --memory ${MEMORY} --vcpus ${VCPUS} \
    --disk path=/kvmpool/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2,format=qcow2,bus=virtio \
