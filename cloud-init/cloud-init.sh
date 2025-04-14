e0e5423ed6f73d78941637dc341922d5a8770539
BlueTufa <github@bluetufa.com>
2025-04-14 11:07:38 -0600

    Provision remotely with Ansible

--- cloud-init/cloud-init.sh
+++ cloud-init/cloud-init.sh
@@ -1,26 +1,79 @@
 #! /bin/bash
 
-export INSTANCE_ID=${1:-"vm-fedora-01"}
-export VCPUS=${2:-2}
-export MEMORY=${3:-4096}
-
-echo "Creating a new instance named ${INSTANCE_ID} with ${VCPUS} VCPU and ${MEMORY} RAM"
 # requires install of virt-install, cloud-image-utils, and cloud-init
 
-[[ -f seed.iso ]] && rm seed.iso
-[[ -f user-data.named.yml ]] && rm  user-data.named.yml
-[[ -f meta-data.named.yml ]] && rm  meta-data.named.yml
+echo "Starting..."
+set -euo pipefail
 
-cloud-init schema --config-file user-data.yml
-cloud-init schema --config-file meta-data.yml
-/bin/cat user-data.yml | sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > user-data.named.yml
-/bin/cat meta-data.yml | sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > meta-data.named.yml
+INSTANCE_ID=${1:-"vm-fedora-01"}
+VCPUS=${2:-2}
+MEMORY=${3:-4096}
+FEDORA_VERSION=${4:-41}
+MEDIA_DIR=${5:-"/media/isos"}
+INSTALL_DIR=${6:-"/kvmpool/images"}
+ARCH="x86_64"
+ISO_TYPE="Cloud"
+BASE_URL="https://download.fedoraproject.org/pub/fedora/linux/releases/${FEDORA_VERSION}/${ISO_TYPE}/${ARCH}/images"
+echo $BASE_URL
+IMAGE_NAME=$(curl -sL "${BASE_URL}/" | \
+  grep -E "Fedora-${ISO_TYPE}-Base-Generic-${FEDORA_VERSION}-1\.[0-9]+\.${ARCH}\.qcow2" | \
+  sed -nE "s/.*(Fedora-${ISO_TYPE}-Base-Generic-${FEDORA_VERSION}-1\.[0-9]+\.${ARCH}\.qcow2).*/\1/p" | sort -V | tail -n 1)
+if [[ -z "$IMAGE_NAME" ]]; then
+  echo "Could not find a valid qcow for the requested release. Check the BASE_URL or network."
+  exit 1
+fi
 
-cloud-localds --filesystem=iso9660 seed.iso user-data.named.yml meta-data.named.yml
+echo "Fedora image name: $IMAGE_NAME"
 
-virt-install --name ${INSTANCE_ID} --memory ${MEMORY} --vcpus ${VCPUS} \
-   --disk path=/kvmpool/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2,format=qcow2,bus=virtio \
-   --disk path=$(pwd)/seed.iso,device=cdrom,bus=sata \
-   --os-variant fedora37 \
-   --graphics none --import --noautoconsole
+CHECKSUM_FILE=$(curl -sL "${BASE_URL}/" | \
+  grep -E "Fedora-${ISO_TYPE}-${FEDORA_VERSION}-1\.[0-9]+\-${ARCH}-CHECKSUM" | \
+  sed -nE "s/.*(Fedora-${ISO_TYPE}-${FEDORA_VERSION}-1\.[0-9]+\-${ARCH}-CHECKSUM).*/\1/p" | sort -V | tail -n 1)
+if [[ -z $CHECKSUM_FILE ]]; then
+  echo "Could not find a valid checksum file.  Refusing to continue without checksums."
+  exit 2
+fi
 
+echo "Fedora checksum file: $CHECKSUM_FILE"
+
+echo "Creating a new instance named ${INSTANCE_ID} with ${VCPUS} VCPU and ${MEMORY} RAM"
+
+download-fedora() {
+  # === Download ===
+  echo "Downloading ISO and checksum file..."
+  curl -o "$MEDIA_DIR/$IMAGE_NAME" "${BASE_URL}/${IMAGE_NAME}"
+  curl -o "$MEDIA_DIR/$CHECKSUM_FILE" "${BASE_URL}/${CHECKSUM_FILE}"
+
+  # === Verify Checksum ===
+  echo "Verifying ISO checksum..."
+  # Extract just the matching line and compare
+  grep "${IMAGE_NAME}" "${CHECKSUM_FILE}" | sha256sum --check
+  if [ $? -eq 0 ]; then
+      echo "Checksum verified successfully!"
+  else
+      echo "Checksum verification failed, refusing to continue without checksums."
+      exit 1
+  fi
+}
+
+if [[ ! -f "${MEDIA_DIR}/$IMAGE_NAME" ]]; then
+  download-fedora
+fi
+
+run-virt-install() {
+  [[ -f seed.iso ]] && rm seed.iso
+  [[ -f user-data.named.yml ]] && rm user-data.named.yml
+  [[ -f meta-data.named.yml ]] && rm meta-data.named.yml
+  
+  cloud-init schema --config-file user-data.yml
+  cloud-init schema --config-file meta-data.yml
+  /bin/cat user-data.yml | sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > user-data.named.yml
+  /bin/cat meta-data.yml | sed "s/\${INSTANCE_ID}/${INSTANCE_ID}/g" > meta-data.named.yml
+  
+  cloud-localds --filesystem=iso9660 seed.iso user-data.named.yml meta-data.named.yml
+  
+  virt-install --name ${INSTANCE_ID} --memory ${MEMORY} --vcpus ${VCPUS} \
+     --disk path=/kvmpool/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2,format=qcow2,bus=virtio \
+     --disk path=$(pwd)/seed.iso,device=cdrom,bus=sata \
+     --os-variant fedora37 \
+     --graphics none --import --noautoconsole
+}
