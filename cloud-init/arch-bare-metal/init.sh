cac1520d77486216d809a417a69ce7ef7b39c7c7
BlueTufa <github@bluetufa.com>
2025-05-08 05:10:52 -0600

    arch bare metal

--- cloud-init/arch-bare-metal/init.sh
+++ cloud-init/arch-bare-metal/init.sh
@@ -7,7 +7,9 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-INSTANCE_ID=${1:-"vm-arch-01"}
+INSTANCE_ID=${1:-"arch-metal-01"}
+SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMwt/pVQKCQHHqxEOdh1/JKqJzIyTPLQpqz/Wno0ECqG badger@catamaran"
+MNT=/mnt/arch
 
 # download
 [[ -f archlinux-bootstrap-x86_64.tar.zst ]] || wget https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-x86_64.tar.zst
@@ -37,14 +39,17 @@ echo $INSTANCE_ID > root.x86_64/etc/hostname
 # update locales
 sudo sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' root.x86_64/etc/locale.gen
 
+# terminal font
+echo "FONT=ter-v22b" >> root.x86_64/etc/vconsole.conf
+
 # edit resolv.conf
 # start with cloudflare, dhcp may override later
 echo nameserver 1.1.1.1 > root.x86_64/etc/resolv.conf
 
 # ssh keys
-mkdir -p root.x86_64/root/.ssh/
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMwt/pVQKCQHHqxEOdh1/JKqJzIyTPLQpqz/Wno0ECqG badger@catamaran" > root.x86_64/root/.ssh/authorized_keys
-chmod 600 root.x86_64/root/.ssh/authorized_keys
+# mkdir -p root.x86_64/root/.ssh/
+# echo $SSH_KEY > root.x86_64/root/.ssh/authorized_keys
+# chmod 600 root.x86_64/root/.ssh/authorized_keys
 
 sudo mkdir -p root.x86_64/etc/systemd/network
 
@@ -57,35 +62,55 @@ Name=$IF_NAME
 DHCP=yes
 EOF
 
-rsync -aAXH root.x86_64/ /mnt/arch/
+rsync -aAXH root.x86_64/ "$MNT"
 
 for i in run proc sys dev
 do echo "Mounting $i"
-  mount --make-rprivate /$i
-  mount --bind /$i /mnt/arch/$i
+  mount --bind /$i "$MNT/$i"
+  mount --make-rslave "$MNT/$i"
 done
 
-chroot /mnt/arch /bin/bash -c "
+chroot "$MNT" /bin/bash -c "
+    swapoff -a
     ln -sf /usr/share/zoneinfo/America/Denver /etc/localtime
     pacman-key --init
     pacman-key --populate archlinux
     locale-gen
+
     echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-    pacman -Sy --noconfirm base linux linux-firmware efibootmgr
-    pacman -Sy --noconfirm systemd dhcpcd openssh
-    pacman -Sy --noconfirm grub os-prober
-
-    systemctl enable dhcpcd
-    systemctl enable systemd-networkd
-    systemctl enable sshd
-    # systemctl enable systemd-resolved
+    # install base system
+    pacman -Sy --noconfirm base linux terminus-font linux-firmware dhcpcd openssh avahi grub os-prober efibootmgr
 
     mkinitcpio -p linux
     genfstab -U / > /etc/fstab
+
+    echo \"root:root\" | chpasswd
+
+    systemctl enable dhcpcd systemd-networkd sshd avahi-daemon.service
+
+    # install user mode packages
+    pacman -Sy --noconfirm man-db eza git nvim htop zsh bat dnsutils trash-cli openbsd-netcat gnupg rsync fastfetch git-delta starship fzf which sudo
+    # install dev tools
+    pacman -Sy --noconfirm base-devel linux-headers
+
+    # optional: nvidia / yubikey hardware
+    # pacman -Sy cuda \
+    #   nvidia \
+    #   nvidia-settings \
+    #   nvidia-utils \
+    #   nvtop \
+    #   yubikey-manager \
+    #   yubikey-personalization
+
+    useradd badger
+    usermod -aG wheel badger
+    mkdir -p /home/badger/.ssh
+    echo $SSH_KEY > /home/badger/.ssh/authorized_keys
+    chown -R badger:badger /home/badger
+    chmod 700 /home/badger/.ssh
+    chmod 600 /home/badger/.ssh/authorized_keys
+    sed -i 's/^#\s*\(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
 "
 
-# manually run:
-# grub-install --target=i386-pc /dev/sdX
-# grub-mkconfig -o /boot/grub/grub.cfg
-# reboot
+# install EFI grub manually
