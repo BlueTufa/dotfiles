6e4b5a40828fa4568f9a9aa71273261767cf189a
BlueTufa <opensource@bluetufa.com>
2026-07-15 20:35:05 -0600

    nix bug

--- nix/install.sh
+++ nix/install.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.config/nix/
 echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 
 nix eval .#homeConfigurations.badger.activationPackage
-home-manager switch --flake .#badger
+nix run home-manager/master -- switch --flake .#badger
